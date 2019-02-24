d674e4018d1085f103be5736b6f5229e71ab51c7
Neeraj Kashyap <neeraj@doc.ai>
2019-02-24 15:27:34 -0800

    Parametrized API port

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const portString = process.env.PORT || '3000';
+const port = parseInt(portString, 10);
 
 app.get('/ping', (req, res) => res.send('ok'));
 
 app.get('/next', (req, res) => {
     const sampleImage = fs.readFileSync('google.png');
-    const sampleImageEncoded = new Buffer(sampleImage).toString('base64');
+    const sampleImageEncoded = new Buffer.from(sampleImage).toString('base64');
     const sampleImageUrl = `data:image/png;base64,${sampleImageEncoded}`;
     return res.status(200).json([
         {
