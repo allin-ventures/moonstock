f41c67c453682f38eb923d1fb5a38aec7e4a3213
Neeraj Kashyap <neeraj@doc.ai>
2019-02-24 16:11:53 -0800

    Added yahoo getHistoricalPrices into mock /next

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 
 const express = require('express');
-
 const bodyParser = require('body-parser');
+
+const yahoo = require('./lib/yahoo.js');
 
 const app = express();
 const portString = process.env.PORT || '3000';
@@ -17,22 +18,18 @@ app.use(bodyParser.json())
 
 app.get('/ping', (req, res) => res.send('ok'));
 
-app.get('/next', (req, res) => {
+app.get('/next', async (req, res) => {
     const sampleImage = fs.readFileSync('google.png');
     const sampleImageEncoded = new Buffer.from(sampleImage).toString('base64');
     const sampleImageUrl = `data:image/png;base64,${sampleImageEncoded}`;
+    const samplePrices = await yahoo.getHistoricalPrices('GOOG');
     return res.status(200).json([
         {
             symbol: "GOOG",
             logo: sampleImageUrl,
             progress: 0.42,
             prediction: {
-                graph: [
-                    {x: 1, y: 1},
-                    {x: 2, y: 5},
-                    {x: 3, y: 4},
-                    {x: 4, y: 3},
-                ],
+                graph: samplePrices,
                 text: "This stock is ok",
             },
         },
@@ -42,7 +39,7 @@ app.get('/next', (req, res) => {
 
 app.post('/action', async (req, res) => {
 
-    const {userToken, symbol, action} = req.body; 
+    const {userToken, symbol, action} = req.body;
 
 
     res.body = {userToken, symbol, action};
