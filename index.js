010f7cfd785550c97b95f8ef04d4f9a10bac82a0
Kartik Thakore <kartikthakore@Kartiks-MacBook-Pro.local>
2019-02-25 16:56:21 -0800

    Adding new routes, error codes and mongodb

--- index.js
+++ index.js
@@ -6,90 +6,143 @@ const bearerToken = require('express-bearer-token');
 const bodyParser = require('body-parser');
 
 if (!process.env.STRIPE_KEY) {
-  console.warn("STRIPE KEY IS MISSING")
+    console.warn("STRIPE KEY IS MISSING")
 }
+
+const MongoClient = require('mongodb').MongoClient;
+
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/moonstock';
+
+
+// Create a new MongoClient
+const client = new MongoClient(MONGO_URL);
 
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const yahoo = require('./lib/yahoo.js');
 
-const crypto = require('crypto');
 
-const app = express();
-const portString = process.env.PORT || '3000';
-const port = parseInt(portString, 10);
+client.connect(function (mongoErr) {
+    if (mongoErr)
+        console.error("CANNOT CONNECT TO MONGO", mongoErr)
+    else {
+        console.log("Mongo connected")
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+        const app = express();
+        const portString = process.env.PORT || '3000';
+        const port = parseInt(portString, 10);
 
-// parse application/json
-app.use(bodyParser.json())
+        // parse application/x-www-form-urlencoded
+        app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(bearerToken());
+        // parse application/json
+        app.use(bodyParser.json())
 
-app.get('/ping', (req, res) => res.send('ok'));
+        app.use(bearerToken());
 
+        app.get('/ping', (req, res) => res.send('ok'));
 
-app.post('/cc',  (req, res) => {
+        app.post('/createUser', (req, res) => {
+            const { email } = req.body;
 
+            // If email don't exist make user
+            const token = "5"
 
-    const {token} = req.body; 
-    console.log(token)
-    stripe.customers.create({
-        description: 'Customer for jenny.rosen@example.com',
-        source: token.tokenId // obtained with Stripe.js
-      }, function(err, customer) {
-        // asynchronously called
-        if (err)
-            console.error(err)
-        console.log(customer)
-     });
-})
+            return res.status(200).json({
+                token
+            })
+        })
 
+        app.post('/cc', (req, res) => {
 
-app.get('/meta', async (req, res) => {
+            if (!req.token || req.token !== '5')
+                 return res.status(403).json({ error: "UNAUTH" })
 
-    console.log("Sending config to app")
+            
+            const { token, metadata } = req.body;
 
-    return res.status(200).json({
-        stripe : {
-            publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-            merchantId: process.env.STRIPE_MERCHANT_KEY,
-        }
-    })
-})
+            metadata["moonstockId"] = 42;
+            
+            // if email exists don't   
+            stripe.customers.create({
+                description: 'Customer for ' + email,
+                source: token.tokenId, // obtained with Stripe.js,
+                metadata
+            }, function (err, customer) {
+                // asynchronously called
+                if (stripeErr)
+                    return res.status(403).json({
+                        error: "CC_FAILED"
+                    })
 
-app.get('/next', async (req, res) => {
-    if (!req.token)
-     return res.status(403).json({error: "UNAUTH"})
-
-    const sampleImage = fs.readFileSync('google.png');
-    const sampleImageEncoded = new Buffer.from(sampleImage).toString('base64');
-    const sampleImageUrl = `data:image/png;base64,${sampleImageEncoded}`;
-    const samplePrices = await yahoo.getHistoricalPrices('GOOG');
-    return res.status(200).json([
-        {
-            symbol: "GOOG",
-            logo: sampleImageUrl,
-            progress: 0.42,
-            prediction: {
-                graph: samplePrices,
-                text: "This stock is ok",
-            },
-        },
-    ]);
-});
+                return res.status(200).json({
+                    "success": true
+                })
+                
 
 
-app.post('/action', async (req, res) => {
+            });
 
-    if (!req.token)
-       return res.status(403).json({error: "UNAUTH"})
+        })
 
-    const {userToken, symbol, action} = req.body; 
 
-    return res.status(200).json( {userToken, symbol, action});
+        app.get('/meta', async (req, res) => {
 
-})
+            console.log("Sending config to app")
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+            return res.status(200).json({
+                stripe: {
+                    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+                    merchantId: process.env.STRIPE_MERCHANT_KEY,
+                }
+            })
+        })
+
+        app.get('/next', async (req, res) => {
+            if (!req.token)
+                return res.status(403).json({ error: "UNAUTH" })
+
+            const sampleImage = fs.readFileSync('google.png');
+            const sampleImageEncoded = new Buffer.from(sampleImage).toString('base64');
+            const sampleImageUrl = `data:image/png;base64,${sampleImageEncoded}`;
+            const samplePrices = await yahoo.getHistoricalPrices('GOOG');
+            return res.status(200).json([
+                {
+                    symbol: "GOOG",
+                    logo: sampleImageUrl,
+                    progress: 0.42,
+                    prediction: {
+                        graph: samplePrices,
+                        text: "This stock is ok",
+                    },
+                },
+            ]);
+        });
+
+
+        app.post('/action', async (req, res) => {
+
+            if (!req.token)
+                return res.status(403).json({ error: "UNAUTH" })
+
+            const creditCardExists = true;
+
+            if (creditCardExists) {
+
+                const { userToken, symbol, action } = req.body;
+
+                return res.status(200).json({ userToken, symbol, action });
+
+            } else {
+                return res.status(403).json({ error: "NO_CC"})
+            }
+
+        })
+
+        app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+    }
+
+// mongo connect
+}, { useNewUrlParser: true })
+
